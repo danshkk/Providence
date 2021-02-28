26b25c0945a2fb9e80a81e358c198d63bd9d2569
danshkk <shkurkodan@gmail.com>
2021-03-01 01:44:24 +0300

    push

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-new Swiper('.customers-slider');
+var swiper = new Swiper('.customers-slider-img', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: true,
+    initialSlide: 1,
+});
+
+var swiper = new Swiper('.customers-slider', {
+    slidesPerView: '1',
+});
+
